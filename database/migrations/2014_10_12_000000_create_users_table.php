5b032e176a66067a418ee82fe1687c40d7695572
unknown <rinku101990@gmail.com>
2022-08-19 03:12:28 +0530

    final commit 4

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password')->unique();
-            $table->text('read_password');
+            $table->string('read_password');
             $table->string('otp')->nullable();
             $table->string('mobile');
             $table->enum('registerAs',['user','admin'])->default('user');
